f2594dfa3f4f9ec823bb90351d47032d90aba043
Jagadeesh Idagal <idagalj@gmail.com>
2024-01-28 14:06:50 +0530

    adde text formating and justification controls

--- index.js
+++ index.js
@@ -14,6 +14,39 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
+  // Text Formating //
+  document.getElementById("bold").addEventListener("click", function() {
+    document.execCommand("bold", false, null);
+  });
+
+  document.getElementById("underline").addEventListener("click", function() {
+    document.execCommand("underline", false, null)
+  });  
+  
+  document.getElementById("italic").addEventListener("click", function() {
+    document.execCommand("italic", false, null);
+  });
+
+  document.getElementById("strikethrough").addEventListener("click", function() {
+    document.execCommand("strikethrough", false, null);
+  });
+
+  // Text alignment //
+  document.getElementById("justify-left").addEventListener("click", function() {
+    document.execCommand("justifyLeft", false, null);
+  });
+
+  document.getElementById("justify-center").addEventListener("click", function() {
+    document.execCommand("justifyCenter", false, null);
+  });
+
+  document.getElementById("justify-right").addEventListener("click", () => {
+    document.execCommand("justifyRight", false, null);
+  });
+
+  document.getElementById("justify").addEventListener("click", () => {
+    document.execCommand("justifyFull", false, null);
+  });
   //Output the editor dom content
   document.getElementById("editor").addEventListener("input", () => {
     const content = document.getElementById("editor").innerHTML;
